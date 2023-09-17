8a360a7c2bd672de2291fd4a6fb6eb3c6c7f6726
Umme Hani Hussain <121793260+ummehanihussain0@users.noreply.github.com>
2023-09-17 11:07:06 +0500

    Add files via upload

---
+++ app2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button title="New Button"/>
+      <h1>New React App</h1>
+      <Button title="Click Me"/>
+    </div>
+  );
+}
+export default App;
